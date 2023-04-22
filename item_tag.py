0840f6b6f15fbea36706e035884472089737c67c
apurvjain22 <75423290+apurvjain22@users.noreply.github.com>
2023-04-23 00:32:45 +0530

    Delete item_tag.py

--- item_tag.py
+++
@@ -1,9 +0,0 @@
-from db import db
-
-
-class ItemTag(db.Model):
-    __tablename__ = "item_tag"
-
-    id = db.Column(db.Integer, primary_key=True)
-    store_id = db.Column(db.Integer, db.ForeignKey("items.id"))
-    item_id = db.Column(db.Integer, db.ForeignKey("tags.id"))
